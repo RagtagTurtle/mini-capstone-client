7cf9b4d4796e0c36fcd300e167f7ea3413e36728
RagtagTurtle <e.russell1792@gmail.com>
2018-05-24 21:38:47 -0500

    added users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
   get '/' => 'client/products#index'
   namespace :client do
     get '/products' => 'products#index'
